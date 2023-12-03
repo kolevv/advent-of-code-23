96f03305c78535f7ce321bd6066253b82a5e93b5
kolevv <kolevv@protonmail.com>
2023-12-03 13:08:02 +0200

    Solution to day 1 part II

--- 1/main.py
+++ 1/main.py
@@ -1,12 +1,13 @@
-def main():
-    inputFile = open("input.txt", "r")
-    lines = inputFile.readlines()
+words_to_numbers = {'one': 'o1e', 'two': 't2o', 'three': 't3e', 'four': 'f4r', 'five': 'f5e', 'six': 's6x', 'seven': 's7n', 'eight': 'e8t', 'nine': 'n9e'}
+
+def parse(lines, part_two = False):
     calibration_values = []
 
     for line in lines:
+        if(part_two == True):
+            line = replace_words(line)
         calibration_values.append(get_calibration_value(line))
     
-    print("Sum of calibration values:")
     print(sum(calibration_values))
 
 def get_calibration_value(line):
@@ -23,7 +24,19 @@ def get_calibration_value(line):
     calibration_value = first_digit + last_digit
     return int(calibration_value)
 
-
+def replace_words(input): 
+    for match, replacement in words_to_numbers.items():
+        input = input.replace(match, replacement)
+    return input
 
 if __name__ == "__main__":
-    main()
+    inputFile = open("1\input.txt", "r")
+    lines =inputFile.readlines()
+
+    print("Part 1 sum of calibration values:")
+    parse(lines)
+
+    print("---------------------------------")
+
+    print("Part 2 sum of calibration values:")
+    parse(lines, True)
